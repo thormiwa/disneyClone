a713843e73ad7a040ba7e367c4a44d9a44b94f7b
thormiwa <thormiwa04@gmail.com>
2021-10-02 19:40:11 +0100

    disneyclone

---
+++ script.js
@@ -0,0 +1,11 @@
+(function($){
+
+    $(document).ready(function() {
+        $(".main-carousel").flickity({
+            wrapAround: true,
+            cellAlign: 'left',
+            contain: true,
+        })
+    })
+
+})(jQuery)
